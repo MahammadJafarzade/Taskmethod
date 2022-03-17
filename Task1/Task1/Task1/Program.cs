0ef7d59795b9ec040c677ebe0092623e7369ebe4
MahammedJafarzade <7a4m9bb@code.edu.az>
2022-03-17 12:00:41 +0400

    Methods

---
+++ Task1/Task1/Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(max(10,50,5,34));
+        }
+        static int max(params int[]array)
+        {
+            int max = array[0];
+            foreach(int number in array)
+            {
+                if(number>max)
+                {
+                    max = number;
+                }
+            }
+            return max;
+        }
+        
+            
+        }
+    }
+
+                   
+
+
---
+++ Task2/Task2/Task2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Binary(5));
+        }
+        static int Binary(int n)
+        {
+            int rem = 0;
+            int rev = 0;
+            int count = 0;
+            for (int i = 0; n>0; i++)
+            {
+                rem = n % 2;
+                n = n / 2;
+                rev = rev * 10 + rem;
+                if(rem==1)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+      
+        }
+    }
+
+
